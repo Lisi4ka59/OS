c3d2f6c663efbd3424f30ee446a6cbcaeb06af01
Lisi4ka <122356446+Lisi4ka59@users.noreply.github.com>
2024-10-23 12:04:04 +0300

    Update shell-test.sh

--- shell/shell-test.sh
+++ shell/shell-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Компиляция программы
-gcc -o main main.c
+gcc -o main shell/main.c
 
 # Создание тестовых файлов
 echo "This is a test file with the word test in it." > testfile.txt
